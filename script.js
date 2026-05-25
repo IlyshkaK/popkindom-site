ecaae612f426b52c1150de597fa3fc5b1a1ccd44
Elijah <maddextv4@gmail.com>
2026-05-25 16:30:01 +0300

    add mobile menu

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ document.querySelectorAll("a[href='#']").forEach((link) => {
     event.preventDefault();
   });
 });
+
+const burgerBtn = document.getElementById("burgerBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+if (burgerBtn && mobileMenu) {
+  burgerBtn.addEventListener("click", () => {
+    burgerBtn.classList.toggle("open");
+    mobileMenu.classList.toggle("open");
+  });
+}
